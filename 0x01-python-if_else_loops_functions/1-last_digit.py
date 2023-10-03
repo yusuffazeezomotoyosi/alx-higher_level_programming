35cf90659e90c632a68e33ba805a532b4b5be26d
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-03 04:51:37 -0700

    das

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
+
 if number > 0:
-    print("{} is positive".format(number))
+    state = "positive"
 elif number == 0:
-    print("{} is zero".format(number))
+    state = "zero"
 else:
-    print("{} is negative".format(number))
+    state = "negative"
+
+print(f"{number} is {state}")
