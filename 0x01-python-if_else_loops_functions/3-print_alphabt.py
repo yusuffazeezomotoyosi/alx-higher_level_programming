f5cd8bcb27730e2b097f533301a8412257c56fe0
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-03 04:45:31 -0700

    slope

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+# question 3
+"""fuck"""
+
 for letter in range(97, 123):
-    if chr(letter) == 'q' or chr(letter) == 'o':
-        continue
-    print("{}".format(chr(letter)), end="")
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
