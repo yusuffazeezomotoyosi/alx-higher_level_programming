3595f557bdf38f27f6b1c39f0e046ce71dd09759
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-03 07:51:37 -0700

    sd

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 != 0:
-        i -= ord('A') - ord('a')
-    print(chr(i), end='')
+for i in range(ord('z'), ord('A') - 1, -1):
+        print(chr(i) + chr(i - 32) if (i - ord('A')) % 2 == 0 else chr(i), end='')
