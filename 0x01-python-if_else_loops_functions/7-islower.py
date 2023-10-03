86e80029c04658fbee879366d49e30a3559af73e
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-03 05:12:17 -0700

    dsa

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 7-islower.py
+
 def islower(c):
     # Check if the Unicode code point is within the range of lowercase letters
-    if 97 <= c <= 122:
+    if 97 <= ord(c) <= 122:
         return True
     else:
         return False
