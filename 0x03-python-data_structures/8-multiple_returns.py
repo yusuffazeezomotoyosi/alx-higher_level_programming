d0a296e7792364db56b702a7de6204918ce71d7e
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-08 05:58:21 -0700

    chi

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 
 def multiple_returns(sentence):
     total = len(sentence)
-    char = sentence[0] if total > 0 else "None"
+    char = sentence[0] if total > 0 else None  # Use None (without quotes) for a None value
     new = total, char
-    return(new)
+    return new  # Remove unnecessary parentheses
+
