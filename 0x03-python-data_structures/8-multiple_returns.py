418953fac33d21e9ada15b62259900ceab6318b4
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-08 06:46:10 -0700

    vb

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,4 +5,3 @@ def multiple_returns(sentence):
     char = sentence[0] if total > 0 else None  # Use None (without quotes) for a None value
     new = total, char
     return new  # Remove unnecessary parentheses
-
