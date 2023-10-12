b3f2f7a13ec502870c260df49ad931e73155348a
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-10-12 06:25:21 -0700

    df

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    response = []
-    for row in matrix:
-        sub_matrix = map(lambda num: num**2, row)
-        response.append(list(sub_matrix))
-    return response
+def mutiply_list_map(my_list=[], number=0):
+    return list(map(lambda item: item * number, my_list))
